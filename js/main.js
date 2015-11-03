1adf44b9120a5ad153565f4ba11cddb7ec5cff9a
alextychkoff@gmail.com <fyfcnfcbz12>
2015-11-03 18:52:48 +0200

    main nav js functionality + responsive

--- js/main.js
+++ js/main.js
@@ -76,85 +76,19 @@ $('.tabs .tabs-nav a').on('click', function (e) {
 new WOW().init();
 //end wow js animations by scrolling
 
+//search icon functionality
+$('.search .icon-search').on('click', function() {
+    $(this).parents('.search').toggleClass('opened');
+});
+//end search icon functionality
+
 //main menu functionality
-mainMenuChange();
-
-function mainMenuChange() {
-    var $navWrapper = $('.mainnav .wrapper'),
-        $nav = $('.mainnav nav'),
-        $search = $('.mainnav .search');
-    var toAddDrop = true;
-    var dropBtn = '<span class="drop-btn"></span>';
-    var drop = '<ul class="drop"></ul>';
-    var $dropBtn = $('.drop-btn');
-    var $drop = $('.drop');
-
-    $(window).on('resize', function () { //tracking on resize
-        var navWrapperWidth = $navWrapper.width(),
-            navWidth = $nav.width(),
-            searchWidth = $search.width(),
-            sumWidth = navWidth + searchWidth;
-        var $lastElement = $nav.find('li').not(':hidden').last();
-        var lastElementWidth = $lastElement.width() + 1;
-
-        if (sumWidth >= navWrapperWidth) {
-            addDrop();
-            console.log($lastElement.text() + " width: " + lastElementWidth);
-            console.log(navWrapperWidth - lastElementWidth);
-            console.log("summWidth: " + sumWidth);
-
-            $lastElement.remove().prependTo('.drop');
-        }
-        if (navWrapperWidth > lastElementWidth + sumWidth && !toAddDrop) {
-            revertDrop();
-        }
-    });
-
-    function addDrop() { //adds a dropdown menu by resizing
-        /*var dropBtn = '<span class="drop-btn"></span>';
-        var drop = '<ul class="drop"></ul>';
-        var $dropBtn = $('.drop-btn');*/
-
-        if(toAddDrop) {
-            $nav.append(dropBtn, drop);
-            toAddDrop = false;
-        }
-
-        $dropBtn.on('click', function () {
-            console.log('drop click');
-        });
-
-        /*if(!$dropBtn.hasClass('opened')) {
-            $dropBtn.on('click', function () {
-                $(this).addClass('opened');
-                $nav.find('.drop').addClass('opened');
-            });
-        } else {
-            $dropBtn.on('click', function () {
-                $(this).removeClass('opened');
-                $nav.find('.drop').removeClass('opened');
-            });
-        }*/
-
-        /*$('.drop-btn').on('click', function () {
-            if(!$(this).hasClass('opened')) {
-                $(this).addClass('opened');
-                $nav.find('.drop').addClass('opened');
-            } else {
-                $(this).removeClass('opened');
-                $nav.find('.drop').removeClass('opened');
-            }
-
-        });*/
-    }
-
-    function revertDrop() { //revert an item from drop to main nav
-        $nav.find($drop).find('li').first().appendTo($nav.find('ul').first()).remove();
-        /*var firstIterm = $nav.find($drop).find('li').first();
-        console.log(firstIterm);*/
-        /*console.log('revert');
-        console.log(toAddDrop);*/
-    }
-
-}
+$('.drop-btn').on('click', function () {
+    console.log('click');
+    $(this).addClass('opened');
+});
+$('.drop-btn.opened').on('click', function () {
+    console.log('click opened');
+    $(this).removeClass('opened');
+});
 //end main menu functionality
