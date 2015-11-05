89357635a1e2cfbfe6feb964cbee38754102308b
alextychkoff@gmail.com <fyfcnfcbz12>
2015-11-05 12:39:28 +0200

    fixes for tabs, contacts selection and others

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ quickContactsSelection('.subheader .quick-contacts');
 quickContactsSelection('.map-section .quick-contacts');
 
 function quickContactsSelection(element) {
-    $(element).find('li').on('click', function () {
+    $(element).find('li').on('mouseover', function () {
         $(this).addClass('active').siblings().removeClass('active');
         var dataTip = $(this).data('tip');
-        $(element).find('.tip').text(dataTip);
+        $(element).find('.tip').text(dataTip).addClass('visible');
+    }).on('mouseout', function () {
+        $(this).removeClass('active');
+        $(element).find('.tip').removeClass('visible');
     });
 }
 //end quick contacts selection
@@ -64,11 +67,17 @@ $('.scroller').click(function(){
 //tabs
 $('.tabs .tabs-nav a').on('click', function (e) {
     e.preventDefault();
-    var tabId = $(this).attr('href');
+    var tabId = $(this).data('tab');
     $(this).parent('li').addClass('active')
         .siblings().removeClass('active');
-    $('.tabs-content .single-tab').removeClass('active').removeAttr('style');
-    $('.tabs-content').find(tabId).fadeIn(350).addClass('active');
+    $('.portfolio-items li').each(function () {
+        $(this).hide();
+        if ($(this).data('tab') == tabId) {
+            $(this).fadeIn(400);
+        } else if (tabId == "all") {
+            $(this).fadeIn(400);
+        }
+    });
 });
 //end tabs
 
