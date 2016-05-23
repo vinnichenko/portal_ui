36dc53c9ac3a0fcd592159d918cc446cd19f7319
Artem <tema@vinnichenko.net>
2016-05-23 20:41:08 +0300

    js uglify, images optimization

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
  */
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var imageop = require('gulp-image-optimization');
+// var imageop = require('gulp-image-optimization');
+var uglify = require('gulp-uglify');
 
 gulp.task('sass', function () {
     gulp.src('scss/main.scss')
@@ -16,12 +17,18 @@ gulp.task('watch', function () {
     gulp.watch('scss/**/*.scss', ['sass']);
 });
 
-gulp.task('images', function(cb) {
-    gulp.src(['images/**/*.png','images/**/*.jpg','images/**/*.gif','images/**/*.jpeg']).pipe(imageop({
+gulp.task('compress', function() {
+    return gulp.src('js/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('js/min'));
+});
+
+/*gulp.task('images', function(cb) {
+    gulp.src(['images/!**!/!*.png','images/!**!/!*.jpg','images/!**!/!*.gif','images/!**!/!*.jpeg']).pipe(imageop({
         optimizationLevel: 5,
         progressive: true,
         interlaced: true
     })).pipe(gulp.dest('images/optimized')).on('end', cb).on('error', cb);
-});
+});*/
 
-gulp.task('default', ['sass', 'watch', 'images']);
+gulp.task('default', ['sass', 'watch', 'compress']);
